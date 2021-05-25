630e1bd830bd647f61958907b1ebff9678212aab
fikrimaulana <maulsana25@gmail.com>
2021-05-25 18:10:47 +0700

    add logo, add login email and google

--- app/src/main/java/app/capstone/bantuansosialdetector/loginandsignup/LoginFragment.kt
+++ app/src/main/java/app/capstone/bantuansosialdetector/loginandsignup/LoginFragment.kt
@@ -1,16 +1,33 @@
 package app.capstone.bantuansosialdetector.loginandsignup
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.firebase.auth.FirebaseAuth
 import app.capstone.bantuansosialdetector.R
+import app.capstone.bantuansosialdetector.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private var user: FirebaseUser? = null
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,24 +40,90 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        auth = FirebaseAuth.getInstance()
-        if (auth.currentUser == null) {
-            auth.signInWithEmailAndPassword("email", "password")
-                .addOnCompleteListener(requireActivity()) { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(requireActivity(), "Successfully Logged In", Toast.LENGTH_LONG)
-                            .show()
-                        //Navigate to Home
-                    } else {
-                        Toast.makeText(requireActivity(), "Login Failed", Toast.LENGTH_LONG).show()
+        auth = Firebase.auth
+        binding?.btnLogin?.setOnClickListener {
+            if (auth.currentUser == null) {
+                auth.signInWithEmailAndPassword("email", "password")
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                requireActivity(),
+                                "Successfully Logged In",
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                            //Navigate to Home
+                             user = auth.currentUser
+                            updateUI(user)
+                        } else {
+                            Toast.makeText(requireActivity(), "Login Failed", Toast.LENGTH_LONG)
+                                .show()
+                        }
                     }
-                }
-        } else {
-            Toast.makeText(requireActivity(), "Already logged in", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(requireActivity(), "Already logged in", Toast.LENGTH_LONG).show()
+            }
         }
+
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        binding?.btnGoogleSignIn?.setOnClickListener {
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account?.id)
+                firebaseAuthWithGoogle(account?.idToken)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener(requireActivity()) { task ->
+            if (task.isSuccessful) {
+                //Navigate to home
+                 user = auth.currentUser
+                updateUI(user)
+            } else {
+                Log.w(TAG, "signInWithCredential:failure", task.exception)
+                updateUI(null)
+            }
+        }
+    }
+
+//    private fun signIn() {
+//        val signInIntent = googleSignInClient.signInIntent
+//        startActivityForResult(signInIntent, RC_SIGN_IN)
+//    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+    }
+
+    companion object {
+        private const val TAG = "LoginFragment"
+        private const val RC_SIGN_IN = 9001
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 }
