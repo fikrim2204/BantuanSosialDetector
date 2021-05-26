5f67caa0b3187adbcc522842a2b810bee5d7785f
fikrimaulana <maulsana25@gmail.com>
2021-05-26 21:16:25 +0700

    add fragment submit and delivery tracking

---
+++ app/src/main/java/app/capstone/bantuansosialdetector/submit/SubmitFragment.kt
@@ -0,0 +1,19 @@
+package app.capstone.bantuansosialdetector.submit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.capstone.bantuansosialdetector.R
+
+class SubmitFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_submit, container, false)
+    }
+}
